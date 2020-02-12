a548cf1203cf9a80e96b38ec87d4ee6811f2fd6c
MY <715711877@qq.com>
2020-02-12 14:45:13 +0800

    重构部分代码

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -6,28 +6,7 @@ Page({
    */
   data: {
     list:[
-      {
-        title:'TypeScript －系统入门到项目实战',
-        img: 'https://img2.mukewang.com/szimg/5e1d990f0885d97306000338-360-202.jpg',
-        author: {
-          img: 'https://img1.sycdn.imooc.com/user/5abe468b0001664107390741-100-100.jpg',
-          name: 'Dell'
-        },
-        date: '2020 / 02 / 11',
-        level: '中级',
-        people : 3761
-      },
-      {
-        title: '2020 重学C++ 重构你的C++知识体系',
-        img: 'https://img1.mukewang.com/szimg/5e26a9f909ef95b512000676-360-202.png',
-        author: {
-          img: 'https://img.mukewang.com/user/545850c80001ebf202200220-100-100.jpg',
-          name: 'quickzhao'
-        },
-        date: '2020 / 02 / 10',
-        level: '中级',
-        people: 179
-      }
+     
     ]
   },
 
@@ -42,7 +21,13 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.request({
+      url: 'https://travel.ismy.wang/api/post_list.json',
+      success: data => {
+        this.setData({list:data.data.list})
+        console.log(data.data.list);
+      }
+    })
   },
 
   /**
