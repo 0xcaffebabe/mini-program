51c252200eee96496716b21a444d51d77be9de99
MY <715711877@qq.com>
2020-02-16 16:20:53 +0800

    增加书籍更多页面

--- pages/books/more-book/more-book.js
+++ pages/books/more-book/more-book.js
@@ -1,51 +1,57 @@
-var util = require("../../util/util.js")
+var util = require("../../../util/util.js")
 Page({
-  onMoreTap(e){
-    wx.navigateTo({
-      url: './more-book/more-book?category='+e.currentTarget.dataset.category,
+  catchScroll(e){
+    console.log("加载更多");
+    this.data.page++;
+    wx.showNavigationBarLoading();
+    this.loadData();
+  },
+  loadData(){
+    wx.request({
+      url: 'https://travel.ismy.wang/api/book/book_category.json?category=' + this.data.category + "?page=" + this.data.page,
+      success: res => {
+        var list = res.data.list;
+        for (let i = 0; i < list.length; i++) {
+          list[i].stars = util.convertRange2Star(list[i].rank);
+        }
+        this.setData({ books: this.data.books.concat(list) });
+        wx.hideNavigationBarLoading();
+      }
     })
   },
   /**
    * 页面的初始数据
    */
   data: {
-   bookList:[]
+    books:[],
+    page:0,
+    category:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      wx.request({
-        url: 'https://travel.ismy.wang/api/book/book.json',
-        success:res=>{
-          
-          var list = res.data.bookList;
-          for (let i =0;i<list.length;i++){
-            
-            for(let j =0;j<list[i].list.length;j++){
-              
-              list[i].list[j].stars = util.convertRange2Star(list[i].list[j].rank);
-            }
-          }
-          console.log(list);
-          this.setData({ bookList:list });
-        }
-      })
+    this.data.category = options.category;
+    wx.setNavigationBarTitle({
+      title: this.data.category,
+    })
+   this.loadData();
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
